2556e10fa5762cdd233fc88df2f5dfc077d8f9ae
FS-Frost <30810344+FS-Frost@users.noreply.github.com>
2024-05-11 15:15:22 -0400

    copy move from line

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,20 @@ function main() {
 }
 
 function copyMove() {
-    const baseLine = "{\\move(320,470,320,168,0,4963)}COSAS";
+    const rawBaseLine = "Dialogue: 0,0:00:00.00,0:00:05.00,Default,,0,0,0,,{\\move(320,470,320,168,0,4963)}COSAS";
 
     const targetLines: string[] = [
-        "{\\pos(182,421)}LINEA 1",
-        "{\\pos(470,361)}LINEA 2"
+        "Dialogue: 0,0:00:00.00,0:00:05.00,Default,,0,0,0,,{\\pos(182,421)}LINEA 1",
+        "Dialogue: 0,0:00:00.00,0:00:05.00,Default,,0,0,0,,{\\pos(470,361)}LINEA 2"
     ];
 
-    let baseItems = asu.parseContent(baseLine);
+    const baseLine = asu.parseLine(rawBaseLine);
+    if (baseLine == null) {
+        console.error("invalid base line", rawBaseLine);
+        return;
+    }
+
+    const baseItems = asu.parseContent(baseLine.content);
     const baseMove = asu.findMove(baseItems);
     if (baseMove == null) {
         console.error("base move not found");
@@ -25,12 +31,18 @@ function copyMove() {
     const deltaY = baseMove.y2 - baseMove.y1;
 
     for (let i = 0; i < targetLines.length; i++) {
-        const line = targetLines[i];
-        const items = asu.parseContent(line);
+        const rawLine = targetLines[i];
+        const line = asu.parseLine(rawLine);
+        if (line == null) {
+            console.error(`line ${i + 1}: invalid line`);
+            break;
+        }
+
+        const items = asu.parseContent(line.content);
         const pos = asu.findPos(items);
         if (pos == null) {
             console.error(`line ${i + 1}: pos not found`);
-            continue;
+            break;
         }
 
         asu.removeTag(items, asu.TagName.pos);
@@ -39,9 +51,10 @@ function copyMove() {
         const x2 = pos.x + deltaX;
         const y2 = pos.y + deltaY;
         asu.setMove(items, x1, y1, x2, y2, baseMove.t1, baseMove.t2);
-        const newLine = asu.contentsToString(items);
-        console.log(newLine);
-        targetLines[i] = newLine;
+        const newContent = asu.contentsToString(items);
+        line.content = newContent;
+        targetLines[i] = asu.lineToString(line);
+        console.log(targetLines[i]);
     }
 }
 
