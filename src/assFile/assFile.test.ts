a87e781883b3c543fc7f0f1f0b04ed4c86848849
FS-Frost <30810344+FS-Frost@users.noreply.github.com>
2024-05-30 19:53:11 -0400

    tests: parse ass file.

--- src/assFile/assFile.test.ts
+++ src/assFile/assFile.test.ts
@@ -28,3 +28,51 @@ test("assFile: parse ass file", async () => {
     expect(assFile.events.lines.length).toEqual(5);
     expect(assFile.extraData.data.length).toEqual(1);
 });
+
+test("assFile: parse karaoke file", async () => {
+    const text = await fs.readFile("test/karaoke.ass", {
+        encoding: "utf-8",
+    });
+
+    const assFile = parseASSFile(text);
+    expect(assFile).not.toBeNull();
+    if (assFile == null) {
+        throw "null ass file";
+    }
+
+    expect(assFile.scriptInfo.comments.length).toEqual(3);
+    expect(assFile.scriptInfo.properties.size).toEqual(11);
+
+    expect(assFile.aegisubProjectGarbage.comments.length).toEqual(0);
+    expect(assFile.aegisubProjectGarbage.properties.size).toEqual(0);
+
+    expect(assFile.styles.styles.length).toEqual(4);
+    expect(assFile.fonts.files.length).toEqual(0);
+    expect(assFile.graphics.files.length).toEqual(0);
+    expect(assFile.events.lines.length).toEqual(62);
+    expect(assFile.extraData.data.length).toEqual(0);
+});
+
+test("assFile: parse mix file", async () => {
+    const text = await fs.readFile("test/mix.ass", {
+        encoding: "utf-8",
+    });
+
+    const assFile = parseASSFile(text);
+    expect(assFile).not.toBeNull();
+    if (assFile == null) {
+        throw "null ass file";
+    }
+
+    expect(assFile.scriptInfo.comments.length).toEqual(3);
+    expect(assFile.scriptInfo.properties.size).toEqual(15);
+
+    expect(assFile.aegisubProjectGarbage.comments.length).toEqual(0);
+    expect(assFile.aegisubProjectGarbage.properties.size).toEqual(0);
+
+    expect(assFile.styles.styles.length).toEqual(7);
+    expect(assFile.fonts.files.length).toEqual(0);
+    expect(assFile.graphics.files.length).toEqual(0);
+    expect(assFile.events.lines.length).toEqual(88);
+    expect(assFile.extraData.data.length).toEqual(0);
+});
